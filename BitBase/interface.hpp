2ac5a782b9f5d0c61c642349651de229c6dd2c40
无北 <conglaiwubei@163.com>
2017-01-25 20:34:39 -0800

    init

---
+++ BitBase/interface.hpp
@@ -0,0 +1,16 @@
+//
+//  interface.hpp
+//  BitBase
+//
+//  Created by lorabit on 25/01/2017.
+//  Copyright © 2017 lorabit. All rights reserved.
+//
+
+#ifndef interface_hpp
+#define interface_hpp
+
+#include <stdio.h>
+
+int start_server();
+
+#endif /* interface_hpp */
---
+++ BitBase/main.cpp
@@ -0,0 +1,8 @@
+
+#include "interface.hpp"
+
+int main(int argc, char *argv[])
+{
+    start_server();
+    return 0;
+}
